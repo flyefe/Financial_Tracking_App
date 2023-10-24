5b9b214744a087f9d7742a3c3d694bbfb57ded03
efe <victorefe5@gmail.com>
2023-10-24 10:48:08 +0100

    template routing effected. app has error. Branched

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, redirect, request
 
+import werkzeug
 views = Blueprint('views', __name__)
 
 @views.route('/')
@@ -8,7 +9,30 @@ def home():
 
 @views.route('/dashboard')
 def dashboard():
-    return "dashboard page"
+    # Sample user information
+    user_info = {
+        'username': 'JohnDoe',
+        'total_expenses': 1000,  # Total expenses for JohnDoe
+        'total_income': 2500,    # Total income for JohnDoe
+    }
+
+    # Sample expenses list
+    expenses = [
+        {'id': 1, 'description': 'Rent', 'amount': 500},
+        {'id': 2, 'description': 'Groceries', 'amount': 200},
+        # Add more expenses as needed
+    ]
+
+    # Sample income list
+    income = [
+        {'id': 1, 'description': 'Salary', 'amount': 2000},
+        {'id': 2, 'description': 'Freelance', 'amount': 500},
+        # Add more income entries as needed
+    ]
+
+    return render_template('dashboard.html', user=user_info, expenses=expenses, income=income)
+
+
 
 @views.route('/profile')
 def profile():
