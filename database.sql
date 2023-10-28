7e4d3d9c2ca458bc284ead0ec3aba880d2e1829d
efe <victorefe5@gmail.com>
2023-10-28 06:22:32 +0100

    final database triggers

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+-- Create a new database
+CREATE DATABASE YourDatabaseName;
+USE YourDatabaseName;
+
 -- Users Table
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -6,79 +10,50 @@ CREATE TABLE users (
     last_name VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
-    date_joined DATETIME DEFAULT CURRENT_TIMESTAMP,
+    date_joined DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Transactions Table
 CREATE TABLE Transactions (
     transaction_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL, FOREIGNKEY
+    user_id INT NOT NULL,
     transaction_type VARCHAR(50),
     amount DECIMAL(15, 2),
     transaction_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     description VARCHAR(255),
-    balance DECIMAL(15, 2)
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-);
-
-
--- Initial
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    date_joined DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-
-
-CREATE TABLE transaction  (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(255),
-    expense FLOAT,
-    income FLOAT,
-    total_income FLOAT,
-    total_expenses FLOAT,
-    balance FLOAT AS (total_income - total_expenses) VIRTUAL,
-    date_recorded DATETIME DEFAULT CURRENT_TIMESTAMP,
-    user_id INT NOT NULL,
+    balance DECIMAL(15, 2),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- Demo
 
--- -- Customers Table
--- CREATE TABLE Customers (
---     customer_id INT AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(255),
---     last_name VARCHAR(255),
---     date_of_birth DATE,
---     address VARCHAR(255),
---     phone_number VARCHAR(15),
---     email VARCHAR(255)
--- );
+-- Trigger for income transactions
+DELIMITER $$
+CREATE TRIGGER income_transaction_trigger
+BEFORE INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+    IF NEW.transaction_type = 'income' THEN
+        SET NEW.total_income = (SELECT IFNULL(SUM(amount), 0) FROM Transactions WHERE user_id = NEW.user_id AND transaction_type = 'income') + NEW.amount;
+        SET NEW.total_expense = (SELECT IFNULL(SUM(amount), 0) FROM Transactions WHERE user_id = NEW.user_id AND transaction_type = 'expense');
+    END IF;
+    
+    SET NEW.balance = NEW.total_income - NEW.total_expense;
+END;
+$$
+DELIMITER ;
 
--- -- Accounts Table
--- CREATE TABLE Accounts (
---     account_id INT AUTO_INCREMENT PRIMARY KEY,
---     customer_id INT,
---     account_number VARCHAR(20),
---     account_type VARCHAR(50),
---     balance DECIMAL(15, 2),
---     date_opened DATE,
---     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
--- );
-
--- -- Transactions Table
--- CREATE TABLE Transactions (
---     transaction_id INT AUTO_INCREMENT PRIMARY KEY,
---     account_id INT,
---     transaction_type VARCHAR(50),
---     amount DECIMAL(15, 2),
---     transaction_date DATETIME,
---     description VARCHAR(255),
---     FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
--- );
+-- Trigger for expense transactions
+DELIMITER $$
+CREATE TRIGGER expense_transaction_trigger
+BEFORE INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+    IF NEW.transaction_type = 'expense' THEN
+        SET NEW.total_income = (SELECT IFNULL(SUM(amount), 0) FROM Transactions WHERE user_id = NEW.user_id AND transaction_type = 'income');
+        SET NEW.total_expense = (SELECT IFNULL(SUM(amount), 0) FROM Transactions WHERE user_id = NEW.user_id AND transaction_type = 'expense') + NEW.amount;
+    END IF;
+    
+    SET NEW.balance = NEW.total_income - NEW.total_expense;
+END;
+$$
+DELIMITER ;
--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, redirect, request, url_for
 from flask_login import current_user, login_required
-from .models import Transactions, Users
+from .models import Transactions
 from . import db
 from flask import flash
 from flask import url_for
-from sqlalchemy import func
 
 views = Blueprint('views', __name__)
 
@@ -41,52 +40,23 @@ def profile():
 
 @views.route('/transaction-history')
 @login_required
-def transaction_history():  
+def transaction_history():
     # Query transactions
-   if True:
-        user_id = 1  # Replace with the user's ID
-        # transaction_id = [result[0] for result in db.session.query(Transactions.transaction_id).all()] # Replace with the transaction ID
-        transaction_id = db.session.query(Transactions.transaction_id).all()
-        
-        transactions = Transactions.query.filter_by(user_id=current_user.id).all()
+    user_id = current_user  # Replace with the user's ID
+    # user_transactions = Transactions.query.filter_by(user_id=user_id).all()
 
-        transaction = db.session.query(Transactions.amount).filter(Transactions.transaction_id == transaction_id).all()
+    transactions = db.session.query(Transactions).filter_by(user_id=user_id).all()
+    
+    # Calculate total income and total expenses
+    # total_income = sum(transaction.income for transaction in transactions)
+    # total_expenses = sum(transaction.expense for transaction in transactions)
+    
+    total_income = db.session.query(Transactions).filter_by(user_id=user_id, transaction_type='Income').all()
+    total_expenses = db.session.query(Transactions).filter_by(user_id=user_id, transaction_type='Expense').all()
+    income = sum(transaction.amount for transaction in total_income)
+    total_expenses  = sum(transaction.amount for transaction in total_expenses)
 
-        transaction_type = Transactions.transaction_type
+    print()
+    # pritnt(trhansactions=user_transactions, total_income=total_income, total_expenses=total_expenses)
+    return render_template('history.html', transactions=transactions, total_income=total_income, total_expenses=total_expenses,)
 
-        for transid in transaction_id:
-            if  transaction_type== 'income':
-                income = (
-                    db.session.query(Transactions.amount)
-                    .filter(
-                        Transactions.user_id == current_user.id,
-                        Transactions.transaction_id == transid,
-                        Transactions.transaction_type == 'income'
-                    )
-                    .scalar()
-                        )
-            else:
-                # Query the expense amount for a specific user and transaction
-                expense = (
-                    db.session.query(Transactions.amount)
-                    .filter(
-                        Transactions.user_id == current_user.id,
-                        Transactions.transaction_id == transid,
-                        Transactions.transaction_type == 'expense'
-                    )
-                    .scalar() )
-           
-        return render_template("history.html", transactions=transactions, income=income, expense=expense, user=current_user)
-    # Render transaction history pagereturn render_template("transaction_history.html", transactions=transactions, user=current_user)
-
-
-@views.route('/test')
-@login_required  
-def test():
-    user = db.session.query(Users).all()
-    print(user)
-    all_transactions = db.session.query(Transactions.transaction_type).all()
-    query = all_transactions
-
-
-    return render_template("addrecord.html", user=current_user, query=query)
