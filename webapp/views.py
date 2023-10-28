ec4aa650957fe0ab45daab6acc606a02b7083af8
efe <victorefe5@gmail.com>
2023-10-28 06:43:53 +0100

    income and expense column sorted

--- webapp/views.py
+++ webapp/views.py
@@ -38,25 +38,38 @@ def profile():
 
 
 
+# @views.route('/transaction-history')
+# @login_required
+# def transaction_history():
+#     # Query transactions
+#     user_id = current_user  # Replace with the user's ID
+#     # user_transactions = Transactions.query.filter_by(user_id=user_id).all()
+
+#     transactions = db.session.query(Transactions).filter_by(user_id=user_id).all()
+    
+#     # Calculate total income and total expenses
+#     # total_income = sum(transaction.income for transaction in transactions)
+#     # total_expenses = sum(transaction.expense for transaction in transactions)
+    
+#     total_income = db.session.query(Transactions).filter_by(user_id=user_id, transaction_type='Income').all()
+#     total_expenses = db.session.query(Transactions).filter_by(user_id=user_id, transaction_type='Expense').all()
+#     income = sum(transaction.amount for transaction in total_income)
+#     total_expenses  = sum(transaction.amount for transaction in total_expenses)
+
+#     print()
+#     # pritnt(trhansactions=user_transactions, total_income=total_income, total_expenses=total_expenses)
+#     return render_template('history.html', transactions=transactions, total_income=total_income, total_expenses=total_expenses,)
+
 @views.route('/transaction-history')
 @login_required
 def transaction_history():
     # Query transactions
-    user_id = current_user  # Replace with the user's ID
-    # user_transactions = Transactions.query.filter_by(user_id=user_id).all()
+    user_id = current_user.id  # Assuming current_user has an 'id' attribute
 
-    transactions = db.session.query(Transactions).filter_by(user_id=user_id).all()
+    transactions = Transactions.query.filter_by(user_id=user_id).all()
     
     # Calculate total income and total expenses
-    # total_income = sum(transaction.income for transaction in transactions)
-    # total_expenses = sum(transaction.expense for transaction in transactions)
-    
-    total_income = db.session.query(Transactions).filter_by(user_id=user_id, transaction_type='Income').all()
-    total_expenses = db.session.query(Transactions).filter_by(user_id=user_id, transaction_type='Expense').all()
-    income = sum(transaction.amount for transaction in total_income)
-    total_expenses  = sum(transaction.amount for transaction in total_expenses)
+    total_income = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'income')
+    total_expenses = sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'expense')
 
-    print()
-    # pritnt(trhansactions=user_transactions, total_income=total_income, total_expenses=total_expenses)
-    return render_template('history.html', transactions=transactions, total_income=total_income, total_expenses=total_expenses,)
-
+    return render_template('history.html', transactions=transactions, total_income=total_income, total_expenses=total_expenses, user=current_user)
